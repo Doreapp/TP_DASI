a0bca78ee300edbede817cdf16f3371144021725
Doreapp <55652808+Doreapp@users.noreply.github.com>
2020-03-30 17:18:08 +0200

    Add files via upload

--- src/main/java/fr/insalyon/dasi/metier/service/Service.java
+++ src/main/java/fr/insalyon/dasi/metier/service/Service.java
@@ -1,22 +1,43 @@
 package fr.insalyon.dasi.metier.service;
 
 import fr.insalyon.dasi.dao.ClientDao;
-import fr.insalyon.dasi.dao.UtilisateurDao;
+import fr.insalyon.dasi.dao.ConversationDao;
 import fr.insalyon.dasi.dao.JpaUtil;
+import fr.insalyon.dasi.dao.UtilisateurDao;
 import fr.insalyon.dasi.metier.modele.Client;
-import fr.insalyon.dasi.metier.modele.Utilisateur;
+import fr.insalyon.dasi.metier.modele.Conversation;
 import fr.insalyon.dasi.metier.modele.Employe;
+import fr.insalyon.dasi.metier.modele.Medium;
+import fr.insalyon.dasi.metier.modele.Utilisateur;
+import java.util.Date;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
- * @author DASI Team
+ * @author Antoine MANDIN + Damien CARREAU
  */
 public class Service {
 
     protected ClientDao clientDao = new ClientDao();
+    protected ConversationDao conversationDao = new ConversationDao();
     protected UtilisateurDao utilisateurDao = new UtilisateurDao();
 
+    /**
+     * Log une erreur 
+     * @param error
+     * @param ex 
+     */
+    public static void Log(String error, Throwable ex){
+        Logger.getAnonymousLogger().log(Level.WARNING, error, ex);
+    }
+    
+    /**
+     * incrit un client dans la base de donnée
+     * @param client Client à insrire
+     * @return l'identifiant du client dans la BDD
+     */
     public Long inscrireClient(Client client) {
         Long resultat = null;
         JpaUtil.creerContextePersistance();
@@ -35,6 +56,12 @@ public class Service {
         return resultat;
     }
 
+    /**
+     * Authentifie l'utilisateur correspondant aux informations
+     * @param mail mail de l'utilisateur
+     * @param motDePasse mot de passe entré
+     * @return l'utilisateur authentifié ou null si erreur d'authentification
+     */
     public Utilisateur authentifierUtilisateur(String mail, String motDePasse) {
         Utilisateur resultat = null;
         JpaUtil.creerContextePersistance();
@@ -55,7 +82,14 @@ public class Service {
         return resultat;
     }
 
+    /**
+     * Créer une conversation (en attente) au sein de la BDD
+     * @param client client de la conversation
+     * @param medium medium lié à la conversation
+     * @return la conversation créée
+     */
     public Conversation creerConversation(Client client, Medium medium) {
+        //TODO : Rechercher l'employé le plus à même de répondre à la conversation
         Conversation conv = new Conversation(
                 Conversation.Etat.EN_ATTENTE,
                 new Date(System.currentTimeMillis()),
@@ -72,32 +106,7 @@ public class Service {
             JpaUtil.validerTransaction();
             conv = null;
         } catch (Exception ex) {
-            Logger.getAnonymousLogger().log(Level.WARNING, "Exception lors de l'appel au Service inscrireClient(client)", ex);
-            JpaUtil.annulerTransaction();
-        } finally {
-            JpaUtil.fermerContextePersistance();
-        }
-        return conv;
-    }
-    
-    public Conversation creerConversation(Client client, Medium medium) {
-        Conversation conv = new Conversation(
-                Conversation.Etat.EN_ATTENTE,
-                new Date(System.currentTimeMillis()),
-                null,
-                medium,
-                null,
-                client
-        );
-
-        JpaUtil.creerContextePersistance();
-        try {
-            JpaUtil.ouvrirTransaction();
-            conversationDao.creer(conv);
-            JpaUtil.validerTransaction();
-            conv = null;
-        } catch (Exception ex) {
-            Logger.getAnonymousLogger().log(Level.WARNING, "Exception lors de l'appel au Service creerConversation(client)", ex);
+            Log("Exception lors de l'appel au Service creerConversation(client, medium)", ex);
             JpaUtil.annulerTransaction();
         } finally {
             JpaUtil.fermerContextePersistance();
@@ -105,9 +114,14 @@ public class Service {
         return conv;
     }
 
+    /**
+     * Recherche les conversations attribuées à l'employé, encore en attente
+     * @param employe 
+     * @return la liste des conversations correspondantes
+     */
     public List<Conversation> rechercherConversationPourEmploye(Employe employe){
         if(employe.getId() == null){
-            System.err.println("ERROR : Service.rechercherConversationPourEmploye ("+employe+") : id null");
+            Log("Exception lors de l'appel au Service rechercherConversationPourEmploye(employé) : ID NULL", new Throwable());
             return null;
         }
         
@@ -117,6 +131,7 @@ public class Service {
             // Recherche des conversation
             resultat = conversationDao.rechercherConversationPourEmploye(employe.getId());
         } catch (Exception ex) {
+            Log("Exception lors de l'appel au Service rechercherConversationPourEmploye(employé)", ex);
             resultat = null;
         } finally {
             JpaUtil.fermerContextePersistance();
@@ -125,9 +140,14 @@ public class Service {
         return resultat;
     }
 
+    /**
+     * Indique que la conversation a débutée
+     * @param c conversation en question
+     * @return si l'opération c'est bien passée
+     */
     public boolean commencerConversation(Conversation c){
         if(c.getId() == null){
-            System.err.println("ERROR : Service.commencerConversation ("+c+") : id null");
+            Log("Exception lors de l'appel au Service commencerConversation(Conversation) : ID NULL", new Throwable());
             return false;
         }
         
@@ -142,7 +162,7 @@ public class Service {
             resultat = conversationDao.mettreAJour(c);
             JpaUtil.validerTransaction();
         } catch (Exception ex) {
-            Logger.getAnonymousLogger().log(Level.WARNING, "Exception lors de l'appel au Service commencerConversation(client)", ex);
+            Log("Exception lors de l'appel au Service commencerConversation(Conversation)", ex);
             JpaUtil.annulerTransaction();
             resultat = false;
         } finally {
@@ -156,9 +176,15 @@ public class Service {
         return resultat;
     }
     
+    /**
+     * Indique que la conversation est finie 
+     * @param c conversation en question
+     * @param commentaire à ajouter à la conversation
+     * @return si l'opération c'est bien passées
+     */
     public boolean finirConversation(Conversation c, String commentaire){
         if(c.getId() == null){
-            System.err.println("ERROR : Service.finirConversation ("+c+") : id null");
+            Log("Exception lors de l'appel au Service finirConversation(Conversation, Commentaire) : ID NULL", new Throwable());
             return false;
         }
         
@@ -175,7 +201,7 @@ public class Service {
             resultat = conversationDao.mettreAJour(c);
             JpaUtil.validerTransaction();
         } catch (Exception ex) {
-            Logger.getAnonymousLogger().log(Level.WARNING, "Exception lors de l'appel au Service finirConversation(client)", ex);
+            Log("Exception lors de l'appel au Service finirConversation(Conversation, Commentaire) : ID NULL", ex);
             JpaUtil.annulerTransaction();
             resultat = false;
         } finally {
