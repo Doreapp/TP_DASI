e203db82ef51383f6f7cd597854ee1eff06531f6
Doreapp <55652808+Doreapp@users.noreply.github.com>
2020-03-30 17:03:00 +0200

    Update Service.java

--- src/main/java/fr/insalyon/dasi/metier/service/Service.java
+++ src/main/java/fr/insalyon/dasi/metier/service/Service.java
@@ -79,4 +79,114 @@ public class Service {
         }
         return conv;
     }
+    
+    public Conversation creerConversation(Client client, Medium medium) {
+        Conversation conv = new Conversation(
+                Conversation.Etat.EN_ATTENTE,
+                new Date(System.currentTimeMillis()),
+                null,
+                medium,
+                null,
+                client
+        );
+
+        JpaUtil.creerContextePersistance();
+        try {
+            JpaUtil.ouvrirTransaction();
+            conversationDao.creer(conv);
+            JpaUtil.validerTransaction();
+            conv = null;
+        } catch (Exception ex) {
+            Logger.getAnonymousLogger().log(Level.WARNING, "Exception lors de l'appel au Service creerConversation(client)", ex);
+            JpaUtil.annulerTransaction();
+        } finally {
+            JpaUtil.fermerContextePersistance();
+        }
+        return conv;
+    }
+
+    public List<Conversation> rechercherConversationPourEmploye(Employe employe){
+        if(employe.getId() == null){
+            System.err.println("ERROR : Service.rechercherConversationPourEmploye ("+employe+") : id null");
+            return null;
+        }
+        
+        List<Conversation> resultat = null;
+        JpaUtil.creerContextePersistance();
+        try {
+            // Recherche des conversation
+            resultat = conversationDao.rechercherConversationPourEmploye(employe.getId());
+        } catch (Exception ex) {
+            resultat = null;
+        } finally {
+            JpaUtil.fermerContextePersistance();
+        }
+        
+        return resultat;
+    }
+
+    public boolean commencerConversation(Conversation c){
+        if(c.getId() == null){
+            System.err.println("ERROR : Service.commencerConversation ("+c+") : id null");
+            return false;
+        }
+        
+        Conversation.Etat before = c.getEtat();
+        c.setEtat(Conversation.Etat.EN_COURS);
+        boolean resultat = false;
+        
+        JpaUtil.creerContextePersistance();
+        try {
+            JpaUtil.ouvrirTransaction();
+            
+            resultat = conversationDao.mettreAJour(c);
+            JpaUtil.validerTransaction();
+        } catch (Exception ex) {
+            Logger.getAnonymousLogger().log(Level.WARNING, "Exception lors de l'appel au Service commencerConversation(client)", ex);
+            JpaUtil.annulerTransaction();
+            resultat = false;
+        } finally {
+            JpaUtil.fermerContextePersistance();
+        }
+        
+        if(!resultat){
+            //on remet les anciennes valeurs
+            c.setEtat(before);
+        }
+        return resultat;
+    }
+    
+    public boolean finirConversation(Conversation c, String commentaire){
+        if(c.getId() == null){
+            System.err.println("ERROR : Service.finirConversation ("+c+") : id null");
+            return false;
+        }
+        
+        Conversation.Etat before = c.getEtat();
+        c.setEtat(Conversation.Etat.TERMINEE);
+        c.setCommentaire(commentaire);
+        
+        boolean resultat = false;
+        
+        JpaUtil.creerContextePersistance();
+        try {
+            JpaUtil.ouvrirTransaction();
+            
+            resultat = conversationDao.mettreAJour(c);
+            JpaUtil.validerTransaction();
+        } catch (Exception ex) {
+            Logger.getAnonymousLogger().log(Level.WARNING, "Exception lors de l'appel au Service finirConversation(client)", ex);
+            JpaUtil.annulerTransaction();
+            resultat = false;
+        } finally {
+            JpaUtil.fermerContextePersistance();
+        }
+        
+        if(!resultat){
+            //on remet les anciennes valeurs
+            c.setEtat(before);
+            c.setCommentaire(null);
+        }
+        return resultat;
+    }
 }
